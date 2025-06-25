ad7c72761aded9de69bf60b245746bd5545ffd42
marina <marina.ivanova@hashicorp.com>
2025-06-25 14:43:09 +0300

    Update acl

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_s3_bucket_acl" "s3_bucket" {
 
   bucket = aws_s3_bucket.s3_bucket.id
 
-  acl = "public-read-write"
+  acl = "aws-exec-read"
 }
 
 resource "aws_s3_bucket_policy" "s3_bucket" {
